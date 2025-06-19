5d33d4a077781822eace683dd428d4c655b95d79
Serroria <merylien.jovanda14@gmail.com>
2025-06-19 18:44:44 +0700

    admin

--- views/admin/admin.php
+++ views/admin/admin.php
@@ -2,22 +2,18 @@
 // File: admin_dashboard.php
 include '../../config/koneksi.php';
 $db = new koneksi();
-$conn = $db->getConnection();
-?>
-
+$conn = $db->getConnection();?>
     <!DOCTYPE html>
     <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Admin</title>
-         <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
             <link rel="stylesheet" href="../../assets/css/admin.css">
             <link rel="stylesheet" href="../../assets/css/adminSidebar.css">
-              <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
+             <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     </head>
     <body>
-        
 
     <!--sidebar-->
      <div class="sidebar">
