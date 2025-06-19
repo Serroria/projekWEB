49e01a35120fa9a4f90002499cc1ec8260cfb842
Serroria <merylien.jovanda14@gmail.com>
2025-06-20 04:29:15 +0700

    .

--- controllers/add to cart/cart_page.php
+++ controllers/add to cart/cart_page.php
@@ -61,6 +61,7 @@ $session_id = session_id();
             </tbody>
         </table>
         <a href="../../index.php" class="btn btn-primary">Lanjut Belanja</a>
+        
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
