565779a7547c04e5a656b224df75361160c0d2f0
Serroria <merylien.jovanda14@gmail.com>
2025-06-19 16:08:20 +0700

    admin

--- views/admin/admin.php
+++ views/admin/admin.php
@@ -14,8 +14,51 @@ if (isset($_GET['edit_id'])) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Admin</title>
             <link rel="stylesheet" href="../../assets/css/admin.css">
+            <link rel="stylesheet" href="../../assets/css/adminSidebar.css">
+             <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     </head>
     <body>
+
+    <!--sidebar-->
+     <div class="sidebar">
+        <div class="logo-details">
+            <div class="logo_name">JamuKita Admin</div>
+            <i class='bx bx-menu' id="btn"></i>
+        </div>
+        <ul class="nav-list">
+            
+            <li>
+                <a href="#" onclick="showContent('products')">
+                    <i class='bx bx-chat'></i>
+                    <span class="links_name">Produk</span>
+                </a>
+                <span class="tooltip">Product</span>
+            </li>
+               <!-- <li>
+                <a href="#" onclick="showContent('orders')">
+                    <i class='bx bx-cart-alt'></i>
+                    <span class="links_name">Order</span>
+                </a>
+                <span class="tooltip">Order</span>
+            </li> -->
+            
+            <li class="profile">
+                <div class="profile-details">
+                    <img src="iconLogo.png" alt="profileImg">
+                    <div class="name_job">
+                        <div class="name">JamuKita</div>
+                        <div class="job">Web Developer</div>
+                    </div>
+                </div>
+               
+                <a  class='bx bx-log-out' id="log_out" href="../login/logout.php" onclick="return confirm('Yakin mau logout?')"> <i class='bx bx-log-out' id="log_out" ></i></a>
+
+            </li>
+        </ul>
+    </div>
+
+    <!--form produk-->
+    <div class="content">
         <h2><?php echo $dataEdit ? 'Edit Produk': 'Tambah Produk'; ?></h2>
     <form class="form-produk" action="../../models/simpanProducts.php" method="POST" enctype="multipart/form-data">
     <?php if ($dataEdit): ?>
@@ -63,11 +106,14 @@ if (isset($_GET['edit_id'])) {
 
 </div>
 
+<!--daftarporduk-->
+
 <?php 
 include 'daftarProduct.php';
 ?>
 
-
+</div>
+<script src="../../assets/js/adminSidebar.js"></script>
 
     </body>
     </html>
