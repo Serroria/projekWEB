f9e001d6b7fd5bbb378c4885768fec87aa64c882
Ariella Chandra Naya <chandranaya02@gmail.com>
2025-06-19 15:04:26 +0700

    homepage

--- assets/js/produk.js
+++ assets/js/produk.js
@@ -1,13 +1,31 @@
-function toggleDesc(id) {
-    const descBox = document.getElementById('descBox-' + id);
+// function toggleDesc(id) {
+//     const descBox = document.getElementById('descBox-' + id);
+//     const arrowIcon = document.getElementById('arrowIcon-' + id);
+
+//     if (descBox.classList.contains('hidden')) {
+//         descBox.classList.remove('hidden');
+//         arrowIcon.innerHTML = '▲';
+//     } else {
+//         descBox.classList.add('hidden');
+//         arrowIcon.innerHTML = '▼';
+//     }
+// }
+
+// function toggleCheckout() {
+//     const modal = document.getElementById('checkoutModal');
+//     modal.classList.toggle('hidden');
+// }
+
+window.toggleDesc = function(id) {
+  const descBox = document.getElementById('descBox-' + id);
     const arrowIcon = document.getElementById('arrowIcon-' + id);
 
     if (descBox.classList.contains('hidden')) {
-        descBox.classList.remove('hidden');
-        arrowIcon.innerHTML = '▲';
+      descBox.classList.remove('hidden');
+      arrowIcon.innerHTML = '▲';
     } else {
-        descBox.classList.add('hidden');
-        arrowIcon.innerHTML = '▼';
+      descBox.classList.add('hidden');
+      arrowIcon.innerHTML = '▼';
     }
 }
 
